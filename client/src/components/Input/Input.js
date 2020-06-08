7db254163bf452e79a5612dc5ac64306593dc304
Soojung Kwak <gsj2464@gmail.com>
2020-06-08 13:59:25 -0400

    initial commit

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Input.css";
+
+const Input = ({ message, setMessage, sendMessage }) => {
+  return (
+    <form action="" className="for">
+      <input
+        className="input"
+        type="text"
+        placeholder="Type a message..."
+        value={message}
+        onChange={({ target: { value } }) => setMessage(value)}
+        onKeyPress={(event) =>
+          event.key === "Enter" ? sendMessage(event) : null
+        }
+      />
+      <button className="sendButton" onClick={(event) => sendMessage(event)}>
+        Send
+      </button>
+    </form>
+  );
+};
+
+export default Input;
